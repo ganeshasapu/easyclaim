607ae0f6fe2ada0b69837220a3220f2d010d7f8c
ron-madan <codewithron@gmail.com>
2023-10-11 21:01:40 -0400

    Adding endpoints to connect backend to frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+//const nextConfig = {}
+//module.exports = nextConfig
 
-module.exports = nextConfig
+module.exports = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'http://localhost:8080/api/:path*',
+          },
+        ]
+      },
+  };
