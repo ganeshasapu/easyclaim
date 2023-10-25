b1153aa093ad49a8d169112bc1335664babf9a66
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2023-10-25 14:23:19 -0400

    made pages and basic routing between

---
+++ frontend/app/inbox/page.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export default function Inbox() {
+    return (
+        <main className="flex h-screen w-full flex-col items-center justify-between">
+            <div className="flex w-full p-4 items-center border-b border-white">
+                <div className="flex w-full items-center justify-evenly p-2 gap-8">
+                    <Link
+                        className="py-2 border rounded w-full px-4 bg-green-50 text-black text-center font-bold"
+                        href={"/inbox"}
+                    >
+                        Inbox
+                    </Link>
+
+                    <Link
+                        className="py-2 border rounded w-full px-4 text-center"
+                        href={"/database"}
+                    >
+                        Database
+                    </Link>
+                </div>
+                <div className="flex">
+                    <div className="rounded-full bg-green-300 w-10 h-10" />
+                </div>
+            </div>
+            <div className="w-full h-full flex">
+                <div className="w-[20vw] h-full bg-gray-50"></div>
+                <div className="w-full h-full text-white p-4">
+                    <h1 className="text-3xl font-bold">Claims to Process</h1>
+                </div>
+            </div>
+        </main>
+    );
+}
