48656fabcce86b68b3a564c7531c820ed8f7128d
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2023-11-25 01:22:18 -0500

    commented out popup to fix error

--- frontend/app/components/Popup/PopupComponent.tsx
+++ frontend/app/components/Popup/PopupComponent.tsx
@@ -3,22 +3,22 @@
 import React, { useState } from 'react';
 import './PopupComponent.css';
 
-const ConfirmationPopup = ({ isOpen, onApprove, onCancel }) => {
-  if (!isOpen) {
-    return null;
-  }
+// const ConfirmationPopup = ({ isOpen, onApprove, onCancel }: ) => {
+//   if (!isOpen) {
+//     return null;
+//   }
 
-  return (
-    <div className="popup-container">
-      <div className="popup">
-        <p>Are you sure you want to approve this claim?</p>
-        <div className="button-container">
-          <button onClick={onApprove}>Yes</button>
-          <button onClick={onCancel}>No</button>
-        </div>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="popup-container">
+//       <div className="popup">
+//         <p>Are you sure you want to approve this claim?</p>
+//         <div className="button-container">
+//           <button onClick={onApprove}>Yes</button>
+//           <button onClick={onCancel}>No</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default ConfirmationPopup;
+// export default ConfirmationPopup;
