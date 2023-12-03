5aaf5c610a8e3720bdea4790d91f15496f5a464f
CaesarSaleh <caesarssaleh@gmail.com>
2023-12-02 19:44:17 -0500

    removed it from env file

--- frontend/app/config/firebase.ts
+++ frontend/app/config/firebase.ts
@@ -1,15 +1,10 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
+import { FirebaseOptions, initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAKbkIpyiL_b1aCVOvRAEXEsddirN65qtY",
-    authDomain: "easyclaim-6b8fa.firebaseapp.com",
-    projectId: "easyclaim-6b8fa",
-    storageBucket: "easyclaim-6b8fa.appspot.com",
-    messagingSenderId: "14365921237",
-    appId: "1:14365921237:web:825843d963c926fc74775c",
-    measurementId: "G-BTYS0YPQKF"
-}
 
-const app = initializeApp(firebaseConfig)
-export const auth = getAuth()
+
+const firebase = process.env.FIREBASE as FirebaseOptions; 
+
+const app = initializeApp(firebase);
+
+export const auth = getAuth(app);
