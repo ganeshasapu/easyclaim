82c45f0c346864da27bb8c82dafacbc785b41040
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2023-12-02 21:42:48 -0500

    updated base_url routing for deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,12 +2,18 @@
 //const nextConfig = {}
 //module.exports = nextConfig
 
+
+
 module.exports = {
     async rewrites() {
         return [
           {
             source: '/api/:path*',
+<<<<<<< Updated upstream
             destination: 'https://easyclaim.azurewebsites.next/api/:path*',
+=======
+            destination: process.env.NEXT_BACKEND_BASE_URL + '/api/:path*',
+>>>>>>> Stashed changes
           },
         ]
       },
