3d992a532b8b4ad24621b3a1ef14634556be598c
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2023-12-02 21:44:57 -0500

    updated next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,11 +9,7 @@ module.exports = {
         return [
           {
             source: '/api/:path*',
-<<<<<<< Updated upstream
-            destination: 'https://easyclaim.azurewebsites.next/api/:path*',
-=======
             destination: process.env.NEXT_BACKEND_BASE_URL + '/api/:path*',
->>>>>>> Stashed changes
           },
         ]
       },
